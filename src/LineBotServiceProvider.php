3991c740d3839f50c6fe08055e3cb66d2bdf8736
Hifi Saputra <hifisaputra1@gmail.com>
2017-12-20 17:40:30 +0700

    update

--- src/LineBotServiceProvider.php
+++ src/LineBotServiceProvider.php
@@ -27,6 +27,6 @@ class LineBotServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->alias(LineBot::class, 'Line');
+        $this->app->alias(Line::class, 'Line');
     }
 }
