c647859e8d8b151b91b1268657256b3816b85738
Hifi Saputra <hifisaputra1@gmail.com>
2017-12-20 13:12:05 +0700

    first commit

---
+++ src/LineBot.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Fei77\LineBot;
+
+class LineBot
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
+}
---
+++ src/config/line-bot.php
@@ -0,0 +1,7 @@
+<?php
+return [
+
+    'line_secret' => env('LINE_SECRET'),
+    'line_token' => env('LINE_TOKEN'),
+
+];
